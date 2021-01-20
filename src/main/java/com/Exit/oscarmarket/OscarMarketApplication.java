cd8a95879d3d6671d7bd11119bf55488b5ebb6af
odromero1717@gmail.com <IbrIbr2020*>
2021-01-19 21:24:35 -0500

    add fist change profile

---
+++ src/main/java/com/Exit/oscarmarket/OscarMarketApplication.java
@@ -0,0 +1,13 @@
+package com.Exit.oscarmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Exit/oscarmarket/OscarMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Exit.oscarmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
